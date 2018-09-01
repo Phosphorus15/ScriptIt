55767598c0ed2673c30b2ae89bed235af0b953a9
Phosphorus15 <steepout@qq.com>
2018-09-01 23:32:33 +0800

    Simplified & Commented the sample

--- src/test/java/naive/editor/EditorTest.java
+++ src/test/java/naive/editor/EditorTest.java
@@ -1,8 +1,10 @@
-package net.steepout.scriptit.impl;
+package naive.editor;
 
 import net.steepout.scriptit.ScriptEventImpl;
 import net.steepout.scriptit.ScriptPlugin;
 import net.steepout.scriptit.ScriptPluginManager;
+import net.steepout.scriptit.impl.IOUtils;
+import net.steepout.scriptit.impl.PythonPluginManager;
 import net.steepout.scriptit.misc.Events;
 
 import javax.swing.*;
@@ -13,7 +15,7 @@ import java.awt.event.KeyEvent;
 /**
  * This is not exactly a 'test', but a example showing how to use the framework
  */
-public class DistributionalTest {
+public class EditorTest {
 
     static {// register aliases for 'events'
         Events.getAliases().put("Editor", "editorEvent");
@@ -26,30 +28,40 @@ public class DistributionalTest {
         public static JTextArea editor;
     }
 
-    static ScriptPluginManager manager = null;
-
     public static void main(String[] args) {
         manager = new PythonPluginManager(); // acquire a plugin manager
         manager.putLobby(Components.class); // put Lobby (named 'Components')
-        Components.frame = new JFrame("Naive Editor - ScriptIt !");
-        final JFrame frame = Components.frame;
-        frame.setSize(300, 500);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        Components.container = frame.getContentPane();
-        Components.editor = new JTextArea("**Hello!* Write it __here_ ~");
-        Font font = Components.editor.getFont();
-        Components.editor.setFont(new Font(font.getName(), Font.PLAIN, 16));
+        // Initialize UI
+        Components.frame = applyFrameDefaults(new JFrame("Naive Editor - ScriptIt !"));
+        Components.container = Components.frame.getContentPane();
+        Components.editor = applyFont(new JTextArea("**Hello!* Write it __here_ ~"));
         JScrollPane scrollPane = new JScrollPane(Components.editor);
         Components.frame.add(scrollPane);
-        setMenuBar(frame);
+        setMenuBar(Components.frame);
+        // Register listener
         Components.editor.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
                 manager.handleEventsAsync(new EditorEvent(Components.editor, e)); // pass event
             }
         });
-        frame.setVisible(true);
+        // Show Window
+        Components.frame.setVisible(true);
+    }
+
+    static ScriptPluginManager manager = null;
+
+    private static JTextArea applyFont(JTextArea textArea) {
+        Font font = textArea.getFont();
+        textArea.setFont(new Font(font.getName(), Font.PLAIN, 16));
+        return textArea;
+    }
+
+    private static JFrame applyFrameDefaults(JFrame frame) {
+        frame.setSize(300, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        return frame;
     }
 
     private static void setMenuBar(JFrame frame) { // register menu bar
@@ -59,16 +71,19 @@ public class DistributionalTest {
         menu.add(item);
         menuBar.add(menu);
         item.addActionListener((event) -> {
+            // load the plugin script & apply it to the plugin manager
             item.setEnabled(false);
             ScriptPlugin plugin = manager.registerPlugin(IOUtils.readAllFromResource("/dtest.py"));
-            JOptionPane.showMessageDialog(Components.frame, "Plugin Load : " + plugin.getName() + "(" + plugin.getVersion() + ")");
+            // show the message of the returned plugin object
+            JOptionPane.showMessageDialog(Components.frame,
+                    "Plugin Load : " + plugin.getName() + "(" + plugin.getVersion() + ")");
         });
         frame.setJMenuBar(menuBar);
     }
 
-    public static class EditorEvent extends ScriptEventImpl {
+    public static class EditorEvent extends ScriptEventImpl { // Customized events
 
-        protected EditorEvent(JComponent editor, KeyEvent event) {
+        EditorEvent(JComponent editor, KeyEvent event) {
             super("editorEvent", createMap(new Entry<>("editor", editor), new Entry<>("keyEvent", event)));
         }
 
--- src/test/resources/dtest.py
+++ src/test/resources/dtest.py
@@ -1,45 +1,60 @@
-pluginc("markleft", "MarkLeft", "1.0.0-SNAPSHOT")
+# this is a plugin script designed for 'Naive Editor'
+# see /src/test/naive/editor/EditorTest.java
 
+pluginc("markleft", "MarkLeft", "1.0.0-SNAPSHOT")  # markleft is really(rarely) good !
+
+# import necessary classes from java
 jimport("javax.swing.JTextPane")
 jimport("javax.swing.JSplitPane")
 jimport("javax.swing.JScrollPane")
+# html escape support
 import cgi
 
+# create the text pane for previewing 'markleft' document
 markleft = JTextPane()
 markleft.setText("Empty")
 markleft.setEditable(False)
-markleft.setContentType("text/html")
+markleft.setContentType("text/html")  # creating special texts using HTML would be easier
+scroll = JScrollPane(markleft)
 
+
+# the subscription of events is at the bottom of this file (due to the limits of python)
 
 def load(event):
+    # change the title
     frame = Components.frame
     frame.setTitle(frame.getTitle().replace("Naive", "MarkLeft"))
-    scroll = JScrollPane(markleft)
+    # fetch the original text editor
     origin = Components.container.getComponent(0)
-    # container.add(scroll)
+    # create vertical split pane
     splitpane = JSplitPane(JSplitPane.VERTICAL_SPLIT)  # create split pane
     splitpane.setLeftComponent(origin)
     splitpane.setRightComponent(scroll)
     splitpane.setDividerLocation(300)
-    frame.setVisible(False)  # refresh screen
-    frame.setContentPane(splitpane)
+    # refresh & reset window
+    frame.setVisible(False)
+    frame.setContentPane(splitpane)  # substitute the window's root panel
     frame.setVisible(True)
+    # start syncing
     sync_pane(Components.editor.getText())
 
 
-def key_event(event):
-    if event.isAWTKeyEvent():
+def key_event(event):  # listen to key event on original editor
+    if event.isAWTKeyEvent():  # this method was defined in java code within the event definition
+        # see naive.editor.EditorTest$EditorEvent#isAWTKeyEvent
         sync_pane(event.get("editor").get().getText())
 
 
+# refresh the 'markleft' preview panel, which was actually an HTML document container
 def sync_pane(text):
     text = cgi.escape(text)  # escape for html
     text = text.replace("\n", "<br/>")  # line separator
-    text = text.replace("**", "<b>").replace("*", "</b>") # bold
-    text = text.replace("__", "<i>").replace("_", "</i>") # italic
+    text = text.replace("**", "<b>").replace("*", "</b>")  # bold
+    text = text.replace("__", "<i>").replace("_", "</i>")  # italic
     print text
     markleft.setText(text)
 
 
+# subscribe to events
 subscribe(Events.Load, load)
 subscribe(Events.Editor, key_event)
